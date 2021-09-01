a71aa4f148a0cd33c26749482a428d31897e8783
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 19:30:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enumeration
+{
+
+    class Program
+    {
+        class Enumeration_Demo
+        {
+            enum MeetingImportance {
+                Trivial,
+                Regular,
+                Critical }
+
+
+            public static void Main(string[] args)
+            {
+                
+                MeetingImportance meet = MeetingImportance.Critical;
+                int value = (int)MeetingImportance.Critical;
+                
+
+
+                if (meet == MeetingImportance.Critical)
+                {
+                    Console.WriteLine("Critical" + value);
+
+                }
+
+                else if (meet == MeetingImportance.Trivial)
+                {
+                    Console.WriteLine("Trivial" + value);
+                }
+
+                else if (meet == MeetingImportance.Regular)
+                {
+                    Console.WriteLine("Regular" + value);
+                }
+            }
+        }
+    }
+}
+//op Critical 2
